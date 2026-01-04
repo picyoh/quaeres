7fbba8d2c1f1f310b72d65e90b3d4a1339b056f5
picyoh <picotyoh@gmail.com>
2026-01-04 11:56:48 +0100

    ci: :green_heart: add astro actions
    add astro actions to deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://picyoh.github.io',
+  base:'/quaeres',
   vite: {
       ssr: {
           noExternal: ["react-icons"],
